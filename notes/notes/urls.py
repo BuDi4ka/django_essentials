93854367ba34f8b34ea9a6be9d5392a014ef89f3
Dmytro Burdeniuk <164885845+BuDi4ka@users.noreply.github.com>
2025-04-01 12:50:17 +0100

    created users app with signup

--- notes/notes/urls.py
+++ notes/notes/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from noteapp import views
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('noteapp.urls')),
+    path('users/', include('users.urls')),
 ]
